0631d3e21357670d4c2452dbe22f7f0517d3b086
mjmj0217 <mjmj0217@naver.com>
2024-01-01 02:08:41 +0900

    feat: 페이지 접근 권한

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("access_token");
+
   // TODO 수정 필요
 
   if (!token) {
